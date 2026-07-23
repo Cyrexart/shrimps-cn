72345b039c600cc91b17535fd09a05b06b201d22
Andrii Demkiv <andriydemkiv14@gmail.com>
2026-07-23 18:10:03 +0200

    Change docs page to show textarea

--- packages/docs/src/app/page.tsx
+++ packages/docs/src/app/page.tsx
@@ -3,10 +3,12 @@
 import { Button } from "@my-scope/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@my-scope/ui/card";
 import { Input } from "@my-scope/ui/input";
+import { Textarea } from "@my-scope/ui/textarea";
+import { Star } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="text-foreground flex min-h-screen flex-col gap-8 bg-background p-8">
+    <main className="text-foreground mx-auto flex min-h-screen max-w-4xl flex-col gap-8 bg-background p-8">
       <section id="buttons" className="flex gap-4">
         <Button>Primary</Button>
         <Button variant="secondary">Secondary</Button>
@@ -25,11 +27,23 @@ export default function Home() {
       </section>
 
       <section id="cards" className="flex gap-4">
-        <Card>
-          <CardHeader>Header</CardHeader>
-          <CardContent>Card Content</CardContent>
-          <CardFooter>Footer</CardFooter>
+        <Card className="max-w-sm">
+          <CardHeader className="text-2xl font-bold">Card Header</CardHeader>
+          <CardContent>
+            This the Card Content section, which can be used for displaying
+            useful(or not) information.
+          </CardContent>
+          <CardFooter className="flex justify-start gap-2">
+            <Button size="sm">Learn more</Button>
+            <Button variant="outline" size="sm">
+              Go Back
+            </Button>
+          </CardFooter>
         </Card>
+      </section>
+      <section id="textareas" className="flex gap-4">
+        <Textarea placeholder="default" />
+        <Textarea placeholder="fixed" className="h-24 resize-none" />
       </section>
     </main>
   );
