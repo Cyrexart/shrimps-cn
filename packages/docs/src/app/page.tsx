f4a58276419f7d58f9a67cb44f07fc7daf512ba8
Andrii Demkiv <andriydemkiv14@gmail.com>
2026-07-23 18:25:45 +0200

    Change docs page to show input group

--- packages/docs/src/app/page.tsx
+++ packages/docs/src/app/page.tsx
@@ -3,10 +3,17 @@
 import { Button } from "@my-scope/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@my-scope/ui/card";
 import { Input } from "@my-scope/ui/input";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@my-scope/ui/input-group";
+import { Textarea } from "@my-scope/ui/textarea";
+import { InfoIcon, SearchIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="text-foreground flex min-h-screen flex-col gap-8 bg-background p-8">
+    <main className="text-foreground mx-auto flex min-h-screen max-w-4xl flex-col gap-8 bg-background p-8">
       <section id="buttons" className="flex gap-4">
         <Button>Primary</Button>
         <Button variant="secondary">Secondary</Button>
@@ -25,11 +32,37 @@ export default function Home() {
       </section>
 
       <section id="cards" className="flex gap-4">
-        <Card>
-          <CardHeader>Header</CardHeader>
-          <CardContent>Card Content</CardContent>
-          <CardFooter>Footer</CardFooter>
+        <Card className="max-w-sm">
+          <CardHeader className="text-2xl font-bold">Card Header</CardHeader>
+          <CardContent>
+            This the Card Content section, which can be used for displaying
+            useful(or not) information.
+          </CardContent>
+          <CardFooter className="flex justify-start gap-2">
+            <Button size="sm">Learn more</Button>
+            <Button variant="outline" size="sm">
+              Go Back
+            </Button>
+          </CardFooter>
         </Card>
+      </section>
+      <section id="textareas" className="flex gap-4">
+        <Textarea placeholder="default" />
+        <Textarea placeholder="fixed" className="h-24 resize-none" />
+      </section>
+      <section id="input-groups" className="flex gap-4">
+        <InputGroup>
+          <InputGroupInput placeholder="search" />
+          <InputGroupAddon align="inline-start">
+            <SearchIcon className="size-4" />
+          </InputGroupAddon>
+        </InputGroup>
+        <InputGroup>
+          <InputGroupInput placeholder="info" />
+          <InputGroupAddon align="inline-end">
+            <InfoIcon className="size-4" />
+          </InputGroupAddon>
+        </InputGroup>
       </section>
     </main>
   );
